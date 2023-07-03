bc8c4acc548ad59eb7daf09003c8e216bdcfbe10
Eternum101 <eternumhd111@gmail.com>
2023-07-03 16:59:39 +1000

    Added checkWeeklyForecast Function in weather.js Added updatedWeeklyForecast Function in weather.js Added toTitleCase Function in weather.js Modified checkCurrentWeather Function in weather.js Added btnMetric & btnImperial in weather.js Added Unit Conversion Functionality in weather.js Modified Button Styling in style.css Modified Table Styling in style.css

--- src/weather.js
+++ src/weather.js
@@ -20,35 +20,54 @@ export function getWeather() {
     const currentDateTime = document.querySelector('[data-current-date]');
     const currentTime = document.querySelector('[data-current-time]');
 
-    const apiKey = "ded01c248c627baaa344663e4262c277";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+    const tableBody = document.querySelector('.weekly-section tbody');
+    const rowTemplate = document.querySelector('#weekly-row-template');
 
     const searchInput = document.querySelector(".search-input input");
     const searchButton = document.querySelector(".search-input button");
+    const btnMetric = document.querySelector('.btn-metric');
+    const btnImperial = document.querySelector('.btn-imperial');
 
-    searchButton.addEventListener('click', () => {
-        checkCurrentWeather(searchInput.value);
+    let units = "metric";
+    let lastSearchedCity = "Brisbane";
+    const apiKey = "ded01c248c627baaa344663e4262c277";
+
+    searchButton.addEventListener("click", () => {
+        if (searchInput.value !== "") {
+            lastSearchedCity = searchInput.value;
+            checkCurrentWeather(searchInput.value);
+            checkWeeklyForecast(searchInput.value);
+        }
+    });
+    
+    btnMetric.addEventListener("click", () => {
+        units = "metric";
+        checkCurrentWeather(lastSearchedCity);
+        checkWeeklyForecast(lastSearchedCity);
+        btnMetric.classList.add('btn-active');
+        btnImperial.classList.remove('btn-active');
+    });
+    
+    btnImperial.addEventListener("click", () => {
+        units = "imperial";
+        checkCurrentWeather(lastSearchedCity);
+        checkWeeklyForecast(lastSearchedCity);
+        btnImperial.classList.add('btn-active');
+        btnMetric.classList.remove('btn-active');
     });
 
     async function checkCurrentWeather(city) {
-        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&units=${units}&q=${city}&appid=${apiKey}`);
         let data = await response.json();
 
-        console.log(data);
         currentLocation.innerHTML = data.name + ", ";
         currentCountry.innerHTML = data.sys.country;
-        currentTemp.innerHTML = Math.round(data.main.temp) + "°C";
-        currentWeatherDesc.innerHTML = data.weather[0].description.toUpperCase();
-        currentFeelDesc.innerHTML = Math.round(data.main.feels_like) + "°C";
-        currentWind.innerHTML = data.wind.speed + " km/h";
+        currentWeatherDesc.innerHTML = toTitleCase(data.weather[0].main);
         currentHumidity.innerHTML = data.main.humidity + "%";
         currentCloudiness.innerHTML = data.clouds.all + "%";
-        currentMinTemp.innerHTML = Math.round(data.main.temp_min) + "°C";
-        currentMaxTemp.innerHTML = Math.round(data.main.temp_max) + "°C";
         currentLat.innerHTML = data.coord.lat; 
         currentLong.innerHTML = data.coord.lon;
         currentWindDegrees.innerHTML = data.wind.deg + "&deg";
-        currentVisiblity.innerHTML = (data.visibility / 1000) + " km";
 
         let sunriseDate = moment.unix(data.sys.sunrise);
         currentSunrise.innerHTML = sunriseDate.format('LT');
@@ -61,7 +80,54 @@ export function getWeather() {
 
         let getTime = moment();
         currentTime.innerHTML = getTime.format('LT');
+
+        let tempUnit = units === "metric" ? "°C" : "°F";
+        currentTemp.innerHTML = Math.round(data.main.temp) + tempUnit;
+        currentFeelDesc.innerHTML = Math.round(data.main.feels_like) + tempUnit;
+        currentMinTemp.innerHTML = Math.round(data.main.temp_min) + tempUnit;
+        currentMaxTemp.innerHTML = Math.round(data.main.temp_max) + tempUnit;
+
+        if (units === "metric") {
+            currentWind.innerHTML = data.wind.speed + " km/h";
+            currentVisiblity.innerHTML = (data.visibility / 1000) + " km";
+        } else {
+            let visibilityInMiles = (data.visibility / 1000) * 0.621371;
+            currentVisiblity.innerHTML = visibilityInMiles.toFixed(1) + " mi";
+            currentWind.innerHTML = data.wind.speed + " mph";
+        }
+}
+    async function checkWeeklyForecast(city) {
+        const weeklyApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+        const response = await fetch(weeklyApiUrl);
+        let data = await response.json();
+
+        updatedWeeklyForecast(data);
+}
+
+    function updatedWeeklyForecast(data) {
+        tableBody.innerHTML = '';
+
+        for (let i = 0; i < data.list.length; i += 8) {
+            const dayData = data.list[i];
+            const row = rowTemplate.content.cloneNode(true);
+        
+            row.querySelector('[data-icon]').src = `http://openweathermap.org/img/w/${dayData.weather[0].icon}.png`;
+            row.querySelector('[data-day]').textContent = moment(dayData.dt_txt).format('dddd');
+            row.querySelector('[data-rain]').textContent = `${dayData.pop * 100}%`;
+            row.querySelector('[data-humidity]').textContent = `${dayData.main.humidity}%`;
+            row.querySelector('[data-min-temp]').textContent = Math.round(dayData.main.temp_min);
+            row.querySelector('[data-max-temp]').textContent = Math.round(dayData.main.temp_max);
+
+            tableBody.appendChild(row);
+        }
     }
 
+    function toTitleCase(str) {
+        return str.replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
     checkCurrentWeather('Brisbane');
+    checkWeeklyForecast('Brisbane');
 }
